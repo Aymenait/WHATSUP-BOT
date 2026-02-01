29fbd00922d2b67705965f3eba3068735fff4cdd
Aymenait <160320210+Aymenait@users.noreply.github.com>
2026-02-01 20:09:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import './whatsapp-handler.js'; // Just import to start the bot
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
